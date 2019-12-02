0a12c78a8f43027bfe045bc2ab39067920cf3549
ranieri-unimi <martinvincente.ranieri@studenti.unimi.it>
2019-12-02 12:25:04 +0100

    Added sides file

---
+++ app/src/main/java/com/example/matrice/FightActivity.java
@@ -0,0 +1,16 @@
+package com.example.matrice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FightActivity extends AppCompatActivity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fight);
+	}
+}
