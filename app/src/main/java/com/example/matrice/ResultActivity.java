769a9d206099bb5453d3463badb6d92c4eadf9b0
ranieri-unimi <martinvincente.ranieri@studenti.unimi.it>
2019-12-03 17:01:45 +0100

    Result design completed

--- app/src/main/java/com/example/matrice/ResultActivity.java
+++ app/src/main/java/com/example/matrice/ResultActivity.java
@@ -1,24 +1,68 @@
 package com.example.matrice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Instrumentation;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
+
+import com.example.matrice.databinding.ActivityProfileBinding;
+import com.example.matrice.databinding.ActivityResultBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ResultActivity extends AppCompatActivity
 {
+	public Smaug h = Smaug.getInstance();
+	public ActivityResultBinding b;
 	public static final String H_LOC = "JSON_SAVED_RESPONSE";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_result);
+		b = DataBindingUtil.setContentView(this, R.layout.activity_result);
+		
+		try {
+			JSONObject j = (JSONObject) h.get(H_LOC);
+			Player p = (Player) h.get(getString(R.string.profile));
+			if(j.getBoolean("died")) {
+				b.lytBackResult.setBackgroundColor(Color.RED);
+				b.txtTitleResult.setTextColor(Color.BLACK);
+				b.txtTitleResult.setText(R.string.died);
+			}
+			int hp = Integer.parseInt(j.getString("lp")) - Integer.parseInt(p.getHp());
+			b.txtHpResult.setText(beauty(hp));
+			int xp = Integer.parseInt(j.getString("xp")) - Integer.parseInt(p.getXp());
+			b.txtXpResult.setText(beauty(xp));
+		}
+		catch (JSONException e){
+			Snackbar.make(b.lytBackResult, getText(R.string.no_ok_data), Snackbar.LENGTH_LONG).show();
+			finish();
+		}
+		
 	}
 	
-	public void onClick(View v)
-	{
+	public String beauty(int value) {
+		String label = "0";
+		if(value > 0) {
+			label = "+ ";
+			label += value;
+		}
+		if (value < 0) {
+			label = "- ";
+			label += Math.abs(value);
+		}
+		return label;
+	}
+	
+	public void onClick(View v) {
 		finish();
 	}
 }
